52b4d3ec8946d711a3ef02cd2a516ee3ac6d215e
sfhome1 <sfhome1@icloud.com>
2014-12-05 08:37:17 -0800

    First Rev Project

--- TempConverter/ViewController.swift
+++ TempConverter/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tempFahrenheitTextField: UITextField!
+    
+    @IBOutlet weak var convertedTextLabel: UILabel!
+    
+    @IBOutlet weak var TempCelsiusTextField: UITextField!
+    
+    @IBOutlet weak var convertedCelsiusLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +28,33 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func convertButtonPressed(sender: UIButton) {
+        let degreeTempTextField = tempFahrenheitTextField.text
+        let numberDegreeTempToInt = degreeTempTextField.toInt()
+        var integerDegreeTemp = numberDegreeTempToInt!
+        let conversionConstant = (integerDegreeTemp - 32) * 5/9
+        let convertedTemp = "\(conversionConstant)"
+        convertedTextLabel.hidden = false
+        convertedTextLabel.text = convertedTemp
+        tempFahrenheitTextField.resignFirstResponder()
+        
+        
+    }
 
+    @IBAction func ConvertCelsiusButtonPressed(sender: UIButton) {
+        let degreeTempTextField = TempCelsiusTextField.text
+        let numberDegreeTempToInt = degreeTempTextField.toInt()
+        var integerDegreeTemp = numberDegreeTempToInt!
+        let conversionConstant = ((integerDegreeTemp * 9/5)+32) + 1
+        let convertedTemp = "\(conversionConstant)"
+        convertedCelsiusLabel.hidden = false
+        convertedCelsiusLabel.text = convertedTemp
+        TempCelsiusTextField.resignFirstResponder()
+        
+        
+        
+        
+        
+    }
 }
 
